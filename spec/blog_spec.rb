79635e68170099d7b5ebb97db4eac91f8679561b
Monier <m.monierx@gmail.com>
2023-05-28 16:07:46 +0300

    contexts

---
+++ spec/blog_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+require 'blog'
+require 'post'
+
+describe Blog do
+  it 'has no posts' do
+    expect(Blog.new).to be_empty
+  end
+
+  context 'with one post' do
+    let(:blog) { Blog.new(posts: [Post.new]) }
+
+    it 'has a post when initialized with one' do
+      expect(blog).not_to be_empty
+    end
+
+    it 'counts the number of posts' do
+      expect(blog.posts_count).to eq 1
+    end
+  end
+end
